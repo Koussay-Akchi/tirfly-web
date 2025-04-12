a90c25e6437629dbfbda03b578f2b8cba15a2344
Koussay Akchi <kaskousakchi@gmail.com>
2025-04-12 21:05:16 +0100

    feat: historique reservations

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -14,9 +14,45 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Repository\VoyageRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ReservationController extends AbstractController
 {
+
+    #[Route('/reservations', name: 'historique_reservations')]
+    public function historiqueReservations(
+        Request $request,
+        ReservationRepository $reservationRepository,
+        PaginatorInterface $paginator,
+        Security $security
+    ): Response {
+        $user = $security->getUser();
+
+        if (!$user) {
+            throw new AccessDeniedException("Vous devez être connecté pour accéder à cette page.");
+        }
+
+        $queryBuilder = $reservationRepository->createQueryBuilder('r')
+            ->where('r.client = :user')
+            ->setParameter('user', $user);
+
+        $query = $queryBuilder->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        $reservations = $query->getResult();
+
+        return $this->render('reservations/historique-reservations.html.twig', [
+            'reservations' => $pagination,
+            'reservationsAll' => $reservations,
+        ]);
+    }
+
 
     #[Route('/admin/reservations', name: 'admin_liste_reservations')]
     public function adminReservations(Request $request, ReservationRepository $reservationRepository, PaginatorInterface $paginator): Response
