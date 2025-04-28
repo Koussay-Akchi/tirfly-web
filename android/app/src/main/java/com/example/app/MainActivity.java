f03be2031a47fce36c208410e920de3099d89e59
Azizlouati1 <azizlouati338@gmail.com>
2025-04-28 13:57:16 +0100

    another one :b

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.app;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.epicshaggy.biometric.NativeBiometric;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // correct the capgo plugin registration
+        // Register the NativeBiometric plugin
+        registerPlugin(NativeBiometric.class);
     }
-
     @Override
     public void onStart() {
         super.onStart();
@@ -20,9 +26,15 @@ public class MainActivity extends BridgeActivity {
         if (intent != null && intent.getAction() != null && intent.getAction().equals(Intent.ACTION_VIEW)) {
             Uri uri = intent.getData();
             if (uri != null) {
+                String url = uri.toString();
+                Log.d(TAG, "Deep link URL: " + url);
                 // Load the deep link URL in the webview
-                this.loadUrl(uri.toString());
+                this.loadUrl(url);
+            } else {
+                Log.e(TAG, "Deep link URI is null");
             }
+        } else {
+            Log.d(TAG, "No deep link intent found");
         }
     }
 
